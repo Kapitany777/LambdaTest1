ce4fec1fc43d762990113d665a8c81a74bca538b
Viktor Török <torokv@vnet.hu>
2021-07-29 08:43:47 +0200

    Adding Test2 method

--- src/eu/braincluster/Main.java
+++ src/eu/braincluster/Main.java
@@ -1,14 +1,22 @@
 package eu.braincluster;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args)
     {
-        var names = Arrays.asList("Pistabá", "Feribá", "Béla", "Juliska");
+        Test1();
+        Test2();
+    }
+
+    private static void Test1()
+    {
+        var names = Arrays.asList("Pistabá", "Feribá", "Béla", "Julis");
 
         // for loop
         for (int i = 0; i < names.size(); i++)
@@ -50,6 +58,41 @@ public class Main {
 
         // Lambda 4
         names.forEach(System.out::println);
+        System.out.println();
+    }
+
+    private static void Test2()
+    {
+        var names = Arrays.asList("Pistabá", "Feribá", "Béla", "Julis");
+        var uppercaseNames = new ArrayList<String>();
+
+        // foreach loop
+        for (var name : names)
+        {
+            uppercaseNames.add(name.toUpperCase());
+        }
+
+        uppercaseNames.forEach(System.out::println);
+        System.out.println();
+
+        // Lambda 1
+        uppercaseNames.clear();
+        names.forEach(name -> uppercaseNames.add(name.toUpperCase()));
+        uppercaseNames.forEach(System.out::println);
+        System.out.println();
+
+        // Lambda 2
+        names.stream()
+                .map(name -> name.toUpperCase())
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        // Lambda 3
+        names.stream()
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+
         System.out.println();
     }
 }
