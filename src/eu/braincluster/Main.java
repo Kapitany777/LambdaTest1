16e8d33c7cda5e588ec8ba583de3f71af970bb44
Viktor Török <torokv@vnet.hu>
2021-07-29 11:05:23 +0200

    Adding Test3 method

--- src/eu/braincluster/Main.java
+++ src/eu/braincluster/Main.java
@@ -5,13 +5,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-public class Main {
+public class Main
+{
 
     public static void main(String[] args)
     {
+        System.out.println("Test 1");
         Test1();
+
+        System.out.println("Test 2");
         Test2();
+
+        System.out.println("Test 3");
+        Test3();
     }
 
     private static void Test1()
@@ -53,7 +63,9 @@ public class Main {
         System.out.println();
 
         // Lambda 3
-        names.forEach(name -> { System.out.println(name); });
+        names.forEach(name -> {
+            System.out.println(name);
+        });
         System.out.println();
 
         // Lambda 4
@@ -91,6 +103,57 @@ public class Main {
         // Lambda 3
         names.stream()
                 .map(String::toUpperCase)
+                .forEach(System.out::println);
+
+        System.out.println();
+    }
+
+    public static void Test3()
+    {
+        var names = Arrays.asList("Pistabá", "Feribá", "Béla", "Julis", "Fatime");
+
+        // Filter 1
+        names.stream()
+                .filter(name -> name.startsWith("F"))
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        // Filter 2
+        var startsWithF =
+                names.stream()
+                        .filter(name -> name.startsWith("F"))
+                        .collect(Collectors.toList());
+
+        startsWithF.stream()
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        // Filter 3 with Predicate
+        Predicate<String> startsWithFCharacter = name -> name.startsWith("F");
+
+        startsWithF =
+                names.stream()
+                        .filter(startsWithFCharacter)
+                        .map(String::toUpperCase)
+                        .collect(Collectors.toList());
+
+        startsWithF.stream()
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        // Filter 4
+        Function<String, Predicate<String>> startsWithLetter =
+                letter -> (name -> name.startsWith(letter));
+
+        var startsWithP =
+                names.stream()
+                        .filter(startsWithLetter.apply("P"))
+                        .collect(Collectors.toList());
+
+        startsWithP.stream()
                 .forEach(System.out::println);
 
         System.out.println();
